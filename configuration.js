7c4dc9137fe53643a36c2c31fd1fff0ee923b593
Beginner-ft <65335364+Beginner-ft@users.noreply.github.com>
2020-10-01 10:08:53 +0530

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -14,6 +14,14 @@ exports.config={
 		jasmine.getEnv().addReporter(new AllureReporter({
 		  resultsDir: 'allure-results'
 		})); 
+	 	jasmine.getEnv().afterEach(function (done) {
+		browser.takeScreenshot().then(function (png) {
+		allure.createAttachment('Screenshot', function () {
+		return new Buffer(png, 'base64')
+		   },'image/png')();
+		done();
+		  })
+		});
 		originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
 		jasmine.DEFAULT_TIMEOUT_INTERVAL = 8000;
 		browser.sleep(3000);
